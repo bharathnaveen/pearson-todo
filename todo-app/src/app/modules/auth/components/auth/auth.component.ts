d6323cf28fb24ad82456bb71822f7ff8399f63af
Bharath Kumar <naveenbharath93@gmail.com>
2018-09-16 18:42:18 +0530

    Update auth.component.ts

--- todo-app/src/app/modules/auth/components/auth/auth.component.ts
+++ todo-app/src/app/modules/auth/components/auth/auth.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'gp-auth',
+  selector: 'todo-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
